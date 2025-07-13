4ecbf2aed40bd9e1aa1d4358050fbf273fc60271
aviv555m <avivm0900@gmail.com>
2025-07-13 19:09:00 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,8 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
+const repoName = 'block-buddy-hub-90'
+
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
